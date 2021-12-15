0dad3333454210b20950ea3021c72d4ec2664071
Dmitry Kolesnikov <dim.kolesnikov@gmail.com>
2021-12-15 13:44:59 +0100

    chore: nearly solves 1. kata

--- filter-api-data.js
+++ filter-api-data.js
@@ -1,3 +1,21 @@
 function filterApiData(apiData, mandatoryKeys) {
-  // Add you solution here
+  // wir laufen über jeden Eintrag in der Schleife
+  let newArray = []
+  for (let i of apiData) {
+    // Ausgabe eines Datensatzes 
+   //console.log(i); 
+   // Von jedem Datensatz bekommen wir nur die keys als Array
+   const dataKeys = Object.keys(i)
+   // Kontrollieren, welche keys rauskommen:
+   //console.log(dataKeys)
+   // Hier wollen wir alle keys in einem neuen Array haben:
+   for (let currentI of mandatoryKeys) {
+   if (dataKeys.includes(currentI)){
+      newArray.push(i)
+   }
+    
+   }
+  }
+  console.log(newArray)
+  return newArray
 }
