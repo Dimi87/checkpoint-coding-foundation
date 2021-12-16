6b3e71e0701e45f190666b6586d1ce90f47e678e
Dmitry Kolesnikov <dim.kolesnikov@gmail.com>
2021-12-16 19:39:37 +0100

    chore: nearly solves 2. cata

--- sum-app.js
+++ sum-app.js
@@ -1,3 +1,27 @@
 class SumApp {
-  // Add you solution here
+   constructor(){
+    const numbers = []
+  }
+
+
+  addNumber(n){
+   push.number(n)
+  }
+
+  getSum() {
+    for (let i = 0; i < numbers.length; i++) {
+       let sumArray = sumArray + numbers[i]
+    }
+    return sumArray
+  }
+
+  reset(){
+  numbers = []
+  }
+
 }
+const sumApp = new SumApp();
+sumApp.reset()
+sumApp.addNumber(2)
+
+//console.log("wo",numbers)
